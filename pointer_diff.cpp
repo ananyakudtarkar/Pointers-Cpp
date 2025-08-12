3e7e3abaa772262815939191cd473f432026990d
ananyakudtarkar <ananyask.03@gmail.com>
2025-08-12 20:14:06 +0530

    Create pointer_diff.cpp

---
+++ pointer_diff.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10 , 20 , 30 , 40 , 50};
+    
+    
+    int* pA1 = &arr[2];
+    int* pA2 = &arr[4];
+    int diff = *pA1 - *pA2;
+    cout << "\nDifference is : " << diff;
+
+  return 0;
+
+}
+/*
+OUTPUT :
+  Difference is : -20
+*/
